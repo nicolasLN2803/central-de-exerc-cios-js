1dd450120e6c084125c2b5c4da5dcc4178c76af2
nicolasLN2803 <nicolaslameiras@gmail.com>
2025-04-03 21:56:56 -0300

    exercícios-de-JS

---
+++ condicionais.js
@@ -0,0 +1,48 @@
+// let numero = prompt("Digite um número:");
+
+// if (numero > 0) {
+
+//     console.log("Positivo.")
+
+// } else {
+
+//     if (numero < 0) {
+
+//         console.log("Negativo")
+//     } else {
+
+//     console.log("É zero.")
+
+// }
+
+// }
+
+// let numero = prompt("Digite sua idade:");
+
+// if (numero >18) {
+
+// console.log("maior de idade");
+
+// } else {
+
+// if(numero <18)
+
+// console.log("menor de idade");
+
+// } exercício 1
+
+
+let numero = prompt ("Digite um número")
+
+function solução (){
+
+let= resto = numero %2
+if (resto===0){
+console.log(par)
+
+} else{
+
+console.log(impar)
+
+}
+}
--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-alert("Hellow World na tela!");
+// alert("Hellow World na tela!");
 
-console.log("Hellow World no console!");
+// console.log("Hellow World no console!");
 
+
+let nome = "Nicolas"; // string;
+const idade = 15; //number;
+var cidade = "São Paulo"
+
+// console.log(nome);
+// console.log(idade);
+// console.log(cidade);
+
+console.log("Meu nome é "  + nome);         /*concatenação*/
+console.log("Tenho " + idade + " anos de idade" );
+console.log("Moro em " + cidade);
+
+// console.log(`Meu nome é: ${nome}`)          /*interpolação*/
+// console.log(`Minha idade é: ${idade}`)
+// console.log(`Minha cidade é: ${cidade}`)
+
+
+console.log ( typeof idade);
+
+let n1 = 20
+let n2 = 10
+let divisão = 20/10
+
+console.log( divisão = n1 / n2)
