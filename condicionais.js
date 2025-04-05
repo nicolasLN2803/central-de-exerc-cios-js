5d642b403fe9a73a0a94e8f0837d008b553c258b
nicolasLN2803 <nicolaslameiras@gmail.com>
2025-04-04 21:50:35 -0300

    atualização dos exercicios de java

--- condicionais.js
+++ condicionais.js
@@ -13,36 +13,165 @@
 
 //     console.log("É zero.")
 
-// }
-
-// }
-
-// let numero = prompt("Digite sua idade:");
-
-// if (numero >18) {
-
-// console.log("maior de idade");
-
-// } else {
-
-// if(numero <18)
-
-// console.log("menor de idade");
-
-// } exercício 1
+// } exemplo
 
 
-let numero = prompt ("Digite um número")
 
-function solução (){
+function exercicio01() {
 
-let= resto = numero %2
-if (resto===0){
-console.log(par)
+debugger;
 
-} else{
+    let numero = prompt("Digite sua idade:");
 
-console.log(impar)
+    if (numero >= 18) {
+
+        console.log("maior de idade");
+
+    } else {
+
+        if (numero < 18)
+
+            console.log("menor de idade");
+    }
 
 }
+
+
+
+
+
+function exercicio02() {
+
+debugger;
+
+    let numero = prompt("Digite um número")
+
+    if(numero >0){
+        alert ("Número positivo")
+    }
+        else{
+            if(numero <0){
+                alert ("Número negativo");
+            }
+        }
 }
+
+
+function exercicio03() {
+
+debugger;
+
+    let numero = prompt("Digite sua nota:");
+
+    if (numero >= 60) {
+
+        console.log("Aprovado.")
+
+    } else
+
+        if (numero < 60)
+
+            console.log("Reprovado.");
+
+}
+
+
+function exercicio04() {
+
+debugger;
+
+    let numero = prompt("Digite um número")
+
+    if (numero > 0)
+
+        console.log("Número é positivo")
+
+        else {
+
+            if (numero < 0) {
+
+                console.log("Negativo")
+                    }    else {
+
+                console.log("É zero.")
+
+}
+            }
+        }
+         
+
+function exercicio05 () {
+
+debugger;
+
+let numero = prompt ("Digite sua idade")
+
+if (numero >= 0){
+    alert ("Você é uma criança");
+    }
+
+else if (numero >= 13){
+    alert ("Você é um adoslecente");
+    }
+
+else if (numero >= 18){
+    alert ("Você é um adulto");
+
+}
+else{
+
+alert ("Idade invalida")
+}
+}
+ 
+
+function exercicio06() {
+
+debugger;
+
+    let numero = prompt("Digite um número")
+
+    let = resto = numero % 2
+
+    if (resto === 0) {
+        alert("O número é par")
+
+    } else {
+
+        alert("O número é impar")
+
+    }
+}
+
+
+
+
+function intermediario01(){
+debugger;
+ 
+let numero1 = prompt ("Digite um número")
+let numero2 = prompt (" Digite um número")
+
+let resultado =  numero1 + numero2
+
+console.log (resultado)
+
+
+}
+intermediario01
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
